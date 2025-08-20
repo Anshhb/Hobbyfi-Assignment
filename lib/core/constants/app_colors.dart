db745096e13d85a7c3a08668a684ed269c007da0
b22me012 <120679862+b22me012@users.noreply.github.com>
2025-08-20 11:56:14 +0530

    colors added

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -14,6 +14,8 @@ class AppColors {
   static const Color green3D = Color(0x2938843D);
 static const Color green3DLightSolid = Color(0xFFE8F5E9); 
   static const Color myColor = Color(0xB21E1E1E);
+  static const Color shadowColor = Color(0x14000000);
+  static const Color borderColor = Color(0x1A000000);
   static const Color blackB2 = Color(0x000000B2);
   static const Color yellow00 = Color(0xffB58B00);
   static const Color yellowbc = Color(0xffF3E6BC);
